0badc67b906b3082868dd0e4220e73ccdac272b3
hsynkas <hanlamaz13@posta.pau.edu.tr>
2015-04-08 16:08:13 +0300

    .cs added

---
+++ Book.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    abstract class Book:ICanbeborrow,ICanbelookup
+    {
+        DateTime givenDate;
+        string writer;
+        string name;
+
+        public string Name
+        {
+            get { return name; }
+        }
+        bool borrowComplete;
+        public bool BorrowComplete
+        {
+            get { return borrowComplete; }
+        }
+        string summary;
+        string ISBN;
+        public Book(string writer,string name,string summary,string ISBN)
+        {
+            this.writer = writer;
+            this.name = name;
+            this.summary = summary;
+            this.ISBN = ISBN;
+        }
+        public virtual string ToString()
+        {
+            return "Name..:" +this.name+ "\nWriter..:" + this.writer;
+        }
+
+        public void BorrowIt(DateTime date)
+        {
+            givenDate = date;
+            borrowComplete = true;
+        }
+
+        public string Lookup()
+        {
+            return summary;
+        }
+
+
+        public void ToReturn()
+        {
+            borrowComplete = false;
+        }
+    }
+}
