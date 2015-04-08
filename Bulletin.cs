8c885fbe48033d43583876547c1986b86b30b59a
hsynkas <hanlamaz13@posta.pau.edu.tr>
2015-04-08 16:08:19 +0300

    .cs added

---
+++ Bulletin.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Bulletin:Book
+    {
+        string type;
+        public Bulletin(string writer,string name,string summary,string ISBN,string type):base(writer,name,summary,ISBN)
+        {
+            this.type = type;
+        }
+        public override string ToString()
+        {
+            return base.ToString()+"\nType..:"+this.type;
+        }
+    }
+}
