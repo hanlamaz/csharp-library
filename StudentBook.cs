a8e264ec25dac367d0d815c964428fc025eb6ce6
hsynkas <hanlamaz13@posta.pau.edu.tr>
2015-04-08 16:09:27 +0300

    .cs added

---
+++ StudentBook.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class StudentBook:Book
+    {
+        string area;
+        public StudentBook(string writer,string name,string summary,string ISBN,string area):base(writer,name,summary,ISBN)
+        {
+            this.area = area;
+        }
+        public override string ToString()
+        {
+            return base.ToString()+"\nArea..:"+this.area;
+        }
+    }
+}
